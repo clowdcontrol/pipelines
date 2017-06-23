ea4a461c4accad5c59f28b19101f21555fe8a293
Lee Tirrell <ltirrell@corticometrics.com>
2017-06-23 12:22:33 -0700

    minor edits

--- abide-fs/get_abide_fs.py
+++ abide-fs/get_abide_fs.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
 
 # requires awscli: install using pip install awscli
-# run from mindcontrol directory
+# usage: python get_abide_fs.py /path/to/subject_file
+# run from derired output directory
 
 import os, sys, errno, subprocess
+
 subjfile = sys.argv[1]
 mcdir = os.getcwd()
 
@@ -18,17 +21,17 @@ def mkdir_p(path):
 datadir = os.path.join(mcdir, 'data')
 mkdir_p(datadir)
 
-text_file = open(mcdir + '/' + subjfile, "r")
-subjids = text_file.readlines()
-for s in range(len(subjids)):
-    temp = subjids[s]
-    subjids[s] = temp[:-1]
+subjects_list = []
+with open(subjfile) as subjects:
+    for subject in subjects:
+        subjects_list.append(subject.strip('\n'))
 
 # get data from amazon s3
 abide_url = 's3://fcp-indi/data/Projects/ABIDE_Initiative/Outputs/'
 fspath = 'freesurfer/5.1/'
 
-for s in range(len(subjids)):
-    outdir = os.path.join(datadir, 'derivatives', 'freesurfer', subjids[s])
-    fsdir = os.path.join(abide_url, fspath, subjids[s])
+for subject in subjects_list:
+    outdir = os.path.join(datadir, 'derivatives', 'freesurfer', subject)
+    fsdir = os.path.join(abide_url, fspath, subj)
+    mkdir_p(outdir)
     os.system('aws s3 cp --recursive --no-sign-request {} {}'.format(fsdir, outdir))
