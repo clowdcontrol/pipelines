a52870792cb33dd1f1aafc3a1d41988f74f22e40
amandakeasson <easson.amanda@gmail.com>
2017-07-04 23:19:52 -0400

    added freesurfer-prep

--- abide-fs/get_abide_fs_qc.py
+++ abide-fs/get_abide_fs_qc.py
@@ -54,5 +54,6 @@ for subject in subjects_list:
 if download_type == "bids" or download_type == "both":
     os.system('docker run -v {}:/data/bids crocodoyle/ibis-bids-qc > bids_qc_output.txt'.format(mcdir))
     os.system('python subjqc.py')
+    os.system('docker run -ti --rm -v $PWD/data:/data clowdcontrol/freesurfer_prep:v0.2')
     os.system('python add_deepqc.py {}'.format(subjfile))
 
